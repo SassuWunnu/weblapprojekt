69133d2edce8f9a896d01aebca9bc0f66add0b00
Evaron <kivesgabor1991@gmail.com>
2015-10-26 15:41:51 +0100

    Css javítások a különböző böngészőméretekre, ha minden igaz most már jól jeleníti meg és nyomja össze a tartalmat az ablak méretéhez képest.

--- Weblap/Public/Kezdolap.php
+++ Weblap/Public/Kezdolap.php
@@ -89,6 +89,52 @@ Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper pe
 Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum, quo ex illud congue option. Nam sonet adolescens ea, est nibh movet comprehensam cu. Congue laoreet efficiendi nam in, eam et dico suavitate, mel persius equidem consulatu ut. Animal principes posidonium ut quo, quas consul no est.
 
 Iriure aliquid et pri, mucius euismod omittam pro ei. Mea putant noluisse ne. Ex erant quando mea. Ad brute elitr alterum pro. Ad rebum copiosae platonem sed, eum no discere consequat, duo mollis appellantur ex.
+          Lorem ipsum dolor sit amet, aperiam prodesset accommodare et per. No quo primis legendos iracundia, an per accusamus incorrupte. Usu in appareat singulis accusata. Nobis euripidis in per, pri cu suas abhorreant, et fugit veniam voluptatum nec. Aeque persecuti id duo.
+
+Et possim bonorum splendide vim, liber ceteros sea ei. Ad accusam ullamcorper eos. Ferri clita ne mei. His ea zril omnium iuvaret. An per latine oportere, vim te esse eius munere. Vel an mutat partiendo.
+
+Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper persequeris. Per officiis constituto ad, ut zril eripuit delenit mei.
+
+Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum, quo ex illud congue option. Nam sonet adolescens ea, est nibh movet comprehensam cu. Congue laoreet efficiendi nam in, eam et dico suavitate, mel persius equidem consulatu ut. Animal principes posidonium ut quo, quas consul no est.
+
+Iriure aliquid et pri, mucius euismod omittam pro ei. Mea putant noluisse ne. Ex erant quando mea. Ad brute elitr alterum pro. Ad rebum copiosae platonem sed, eum no discere consequat, duo mollis appellantur ex.
+Lorem ipsum dolor sit amet, aperiam prodesset accommodare et per. No quo primis legendos iracundia, an per accusamus incorrupte. Usu in appareat singulis accusata. Nobis euripidis in per, pri cu suas abhorreant, et fugit veniam voluptatum nec. Aeque persecuti id duo.
+
+Et possim bonorum splendide vim, liber ceteros sea ei. Ad accusam ullamcorper eos. Ferri clita ne mei. His ea zril omnium iuvaret. An per latine oportere, vim te esse eius munere. Vel an mutat partiendo.
+
+Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper persequeris. Per officiis constituto ad, ut zril eripuit delenit mei.
+
+Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum, Lorem ipsum dolor sit amet, aperiam prodesset accommodare et per. No quo primis legendos iracundia, an per accusamus incorrupte. Usu in appareat singulis accusata. Nobis euripidis in per, pri cu suas abhorreant, et fugit veniam voluptatum nec. Aeque persecuti id duo.
+
+Et possim bonorum splendide vim, liber ceteros sea ei. Ad accusam ullamcorper eos. Ferri clita ne mei. His ea zril omnium iuvaret. An per latine oportere, vim te esse eius munere. Vel an mutat partiendo.
+
+Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper persequeris. Per officiis constituto ad, ut zril eripuit delenit mei.
+
+Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum, quo ex illud congue option. Nam sonet adolescens ea, est nibh movet comprehensam cu. Congue laoreet efficiendi nam in, eam et dico suavitate, mel persius equidem consulatu ut. Animal principes posidonium ut quo, quas consul no est.
+
+Iriure aliquid et pri, mucius euismod omittam pro ei. Mea putant noluisse ne. Ex erant quando mea. Ad brute elitr alterum pro. Ad rebum copiosae platonem sed, eum no discere consequat, duo mollis appellantur ex.
+Lorem ipsum dolor sit amet, aperiam prodesset accommodare et per. No quo primis legendos iracundia, an per accusamus incorrupte. Usu in appareat singulis accusata. Nobis euripidis in per, pri cu suas abhorreant, et fugit veniam voluptatum nec. Aeque persecuti id duo.
+
+Et possim bonorum splendide vim, liber ceteros sea ei. Ad accusam ullamcorper eos. Ferri clita ne mei. His ea zril omnium iuvaret. An per latine oportere, vim te esse eius munere. Vel an mutat partiendo.
+
+Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper persequeris. Per officiis constituto ad, ut zril eripuit delenit mei.
+
+Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum, Lorem ipsum dolor sit amet, aperiam prodesset accommodare et per. No quo primis legendos iracundia, an per accusamus incorrupte. Usu in appareat singulis accusata. Nobis euripidis in per, pri cu suas abhorreant, et fugit veniam voluptatum nec. Aeque persecuti id duo.
+
+Et possim bonorum splendide vim, liber ceteros sea ei. Ad accusam ullamcorper eos. Ferri clita ne mei. His ea zril omnium iuvaret. An per latine oportere, vim te esse eius munere. Vel an mutat partiendo.
+
+Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper persequeris. Per officiis constituto ad, ut zril eripuit delenit mei.
+
+Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum, quo ex illud congue option. Nam sonet adolescens ea, est nibh movet comprehensam cu. Congue laoreet efficiendi nam in, eam et dico suavitate, mel persius equidem consulatu ut. Animal principes posidonium ut quo, quas consul no est.
+
+Iriure aliquid et pri, mucius euismod omittam pro ei. Mea putant noluisse ne. Ex erant quando mea. Ad brute elitr alterum pro. Ad rebum copiosae platonem sed, eum no discere consequat, duo mollis appellantur ex.
+Lorem ipsum dolor sit amet, aperiam prodesset accommodare et per. No quo primis legendos iracundia, an per accusamus incorrupte. Usu in appareat singulis accusata. Nobis euripidis in per, pri cu suas abhorreant, et fugit veniam voluptatum nec. Aeque persecuti id duo.
+
+Et possim bonorum splendide vim, liber ceteros sea ei. Ad accusam ullamcorper eos. Ferri clita ne mei. His ea zril omnium iuvaret. An per latine oportere, vim te esse eius munere. Vel an mutat partiendo.
+
+Doming menandri cu vim. Noster sensibus qualisque ex usu. Te vim ipsum semper persequeris. Per officiis constituto ad, ut zril eripuit delenit mei.
+
+Mel accumsan eloquentiam complectitur at, at sea tation civibus argumentum,   
             </p>
 </div>
 
