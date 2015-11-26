16d9b40d0cc359af17af79421edccdd2d13e06fc
Evaron <kivesgabor1991@gmail.com>
2015-11-26 15:04:03 +0100

    Táblákat létrehozó sql kód hozzá lett adva és egy gombbal vezérelhető a kezdőlapról.

--- Weblap/Protected/Model/class.db.php
+++ Weblap/Protected/Model/class.db.php
@@ -1,7 +1,6 @@
 <?php
 
 class dbkezelo {
-
     function kapcsolatnyit() {
         $kapcsolat = new PDO('mysql:dbname=shopdatabase;host=localhost', 'root', '');
         $kapcsolat->exec("SET NAMES utf8");
@@ -13,11 +12,53 @@ class dbkezelo {
     }
 
     function letrehozo() {
-        $sqlstring=""; //ide kéne leírni az össze tábla generálást, de a terv még vátozhat
+        //$adatbazisletrehozo="CREATE SCHEMA IF NOT EXISTS shopdatabase DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci ;USE `shopdatabase`;";
+        $tablaksqlstring="
+                Create table IF NOT EXISTS Felhasznalok(
+                    id int primary key auto_increment not null,
+                    email varchar(45) not null,
+                    felhasznalonev varchar(25) not null,
+                    nev varchar(55) null,
+                    cim varchar(250) null,
+                    telefonszam numeric null,
+                    lastlogin date null);
+                 Create table IF NOT EXISTS Rendelesek(
+                    id int primary key auto_increment not null, 
+                    szoveg varchar(255) not null, 
+                    meret varchar(10) not null, 
+                    datum date not null, 
+                    statusz varchar(45) not null, 
+                    tipus varchar(20) not null, 
+                    teljesites date, 
+                    felhasznalok_id int not null, 
+                    constraint fk_Rendelesek_felhasznalok_id foreign key (felhasznalok_id) References Felhasznalok(id) );
+                 create table if not exists Jelszavak(
+                    id int primary key auto_increment not null,
+                    jelszo long varbinary,
+                    felhasznalok_id int,
+                    constraint fk_Jelszavak_felhasznalok_id foreign key (felhasznalok_id) References Felhasznalok(id));
+                Create table IF NOT EXISTS Regisztracio(
+                    id int primary key auto_increment not null,
+                    email varchar(45) not null,
+                    felhasznalonev varchar(25) not null,
+                    nev varchar(55) null,
+                    cim varchar(250) null,
+                    telefonszam numeric null,
+                    lejar date not null,
+                    regkod numeric);
+                create table if not exists Munkak(
+                    id int primary key auto_increment not null,
+                    kep blob not null,
+                    elado boolean not null,
+                    leiras varchar(255),
+                    keszult date,
+                    meret varchar(15),
+                    ar numeric);";
         $kapcsolat = $this->kapcsolatnyit();
-        $letrehozas = $kapcsolat->prepare(":create");
-        $letrehozas->bindParam(':create', $sqlstring);
-        $letrehozas->execute();
+        $letrehozas = $kapcsolat->prepare($tablaksqlstring);
+        $siker=$letrehozas->execute();
+        //$this->kapcsolatzar($kapcsolat);
+        return $siker;
     }
 
 }
--- Weblap/Protected/View/kezdolap/kezdolap.php
+++ Weblap/Protected/View/kezdolap/kezdolap.php
@@ -7,5 +7,28 @@
 <p>Szerver idő:</p>
 <?php
 echo date('H:i, jS F Y');
-?>                     
+?>  
+<br>
+<br>
+<br>
+<br>
+
+<form aciton="" method="POST">
+<p>Futtatáshoz szükséges egy adatbázis shopdatabase néven utf8-hungarian kódolással (phpmyadmin)</p>
+<button type="submit" name="dbtablagomb">adatbázis tábla létrerehozó</button>
+
+</form>
+
+<?php 
+require 'Protected/Model/class.db.php';
+if($_SERVER["REQUEST_METHOD"]==="POST"){
+    $adatbazis=new dbkezelo();
+    $sikeres=$adatbazis->letrehozo();
+    if($sikeres){
+        echo "Sikeres táblalétrehozás!";
+    }else{
+        echo "Valami balul sült el!";
+    }
+}
+?>
 </div>   
